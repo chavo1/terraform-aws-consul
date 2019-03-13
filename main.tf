6b87b82938333138bb6080406d2734493e375aee
chavo1 <chavdar.rakov@gmail.com>
2019-03-13 15:18:12 +0200

    refactor

--- main.tf
+++ main.tf
@@ -8,10 +8,14 @@ resource "aws_instance" "server" {
   ami                         = "${var.ami}"
   instance_type               = "${var.instance_type}"
   key_name                    = "${var.key_name}"
-  count                       = 2
-  private_ip                  = "172.31.16.1${count.index + 1}"
+  count                       = 3
+  private_ip                  = "172.31.16.${count.index + 11}"
   subnet_id                   = "${var.subnet}"
   associate_public_ip_address = true
+
+  tags {
+    Name = "consul-server0${count.index + 1}"
+  }
 
   connection {
     user        = "ubuntu"
@@ -31,6 +35,45 @@ resource "aws_instance" "server" {
   }
 }
 
-output "public_dns" {
+resource "aws_instance" "client" {
+  ami                         = "${var.ami}"
+  instance_type               = "${var.instance_type}"
+  key_name                    = "${var.key_name}"
+  count                       = 2
+  private_ip                  = "172.31.16.${count.index + 21}"
+  subnet_id                   = "${var.subnet}"
+  associate_public_ip_address = true
+
+  tags {
+    Name = "consul-client0${count.index + 1}"
+  }
+
+  connection {
+    user        = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
+  }
+
+  provisioner "file" {
+    source      = "scripts/"
+    destination = "/tmp/"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo chmod +x /tmp/consul.sh",
+      "sudo chmod +x /tmp/kv.sh",
+      "sudo chmod +x /tmp/nginx.sh",
+      "sudo /tmp/consul.sh",
+      "sudo /tmp/kv.sh",
+      "sudo /tmp/nginx.sh"
+    ]
+  }
+}
+
+output "public_dns_servers" {
   value = "${aws_instance.server.*.public_dns}"
+}
+output "public_dns_clients" {
+  value = "${aws_instance.client.*.public_dns}"
 }
