a234cc3bf80af16faae1f9d084e16e04279fdf22
chavo1 <chavdar.rakov@gmail.com>
2019-03-19 10:24:59 +0200

    add-tests

--- example/test/integration/default/check_state.rb
+++ example/test/integration/default/check_state.rb
@@ -1,5 +1,20 @@
 describe command('terraform state list') do
-  its('stdout') { should eq "module.consul-terraform.aws_instance.client\nmodule.consul-terraform.aws_instance.server\n" }
-  its('stderr') { should eq '' }
+  its('stdout') { should include "module.consul-terraform.aws_instance.client[0]" }
+  its('stderr') { should include '' }
+  its('exit_status') { should eq 0 }
+end
+describe command('terraform state list') do
+  its('stdout') { should include "module.consul-terraform.aws_instance.server[0]" }
+  its('stderr') { should include '' }
+  its('exit_status') { should eq 0 }
+end
+describe command('terraform state list') do
+  its('stdout') { should include "module.consul-terraform.aws_instance.client[1]" }
+  its('stderr') { should include '' }
+  its('exit_status') { should eq 0 }
+end
+describe command('terraform state list') do
+  its('stdout') { should include "module.consul-terraform.aws_instance.server[1]" }
+  its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
